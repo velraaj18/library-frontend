fed1b97b8c561f696cd0988b4ca93835156da3b7
unknown <velraaj30@gmail.com>
2025-06-09 18:35:40 +0530

    Updated the register and login page with Toast

--- Library/src/components/Login.tsx
+++ Library/src/components/Login.tsx
@@ -1,44 +1,106 @@
 import { useState } from "react";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { useRef } from "react";
 
+import { Toast } from "primereact/toast";
+import { Card } from "primereact/card";
+import { InputText } from "primereact/inputtext";
+import { Button } from "primereact/button";
+import { Message } from "primereact/message";
+import { Password } from "primereact/password";
 
 const Login = () => {
+  const navigate = useNavigate();
 
-    const [formData, setFormData] = useState({
-        email : "",
-        password : ""
+  const toast = useRef<Toast>(null);
+
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+ 
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
     });
+  };
 
-    const [message, setMessage] = useState<string>('');
-
-    const handleChange = (e : React.ChangeEvent<HTMLInputElement>) => {
-      e.preventDefault();
-      setFormData({
-        ...formData, [e.target.name] : e.target.value
-      })
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(
+        "http://localhost:5001/api/auth/login",
+        formData
+      );
+      toast.current?.show({
+        severity: "success",
+        detail: response.data.message || "Logged in successfully",
+        summary: "success",
+        life: 5000,        
+      });
+      console.log(response.data.message)
+      setFormData({ email: "", password: "" }); // Clear form
+    } catch (error: any) {
+      toast.current?.show({
+        severity: "error",
+        detail: error.response?.data?.message || "Something went wrong",
+        summary: "error",
+        life: 5000,
+      });
+      console.log(error.response?.data?.message)
     }
-
-    const handleSubmit = async (e : React.FormEvent<HTMLFormElement>) => {
-      e.preventDefault();
-      try {
-        const response = await axios.post("http://localhost:5001/api/auth/login", formData);
-        setMessage(response?.data?.message)
-      } catch (error : any) {
-        setMessage(error.response?.data?.message || "Something went wrong");
-      }
-      
-    }
+  };
 
   return (
     <>
-      <form onSubmit={handleSubmit}>
-          <input type="text" name = "email" placeholder="Email" onChange={handleChange}/> <br />
-          <input type="text" name = "password" placeholder="Password" onChange={handleChange}/> <br />
-          <button type="submit">Submit</button><br />
-      </form>
-      <p>{message}</p>
+      <Toast ref={toast} position="bottom-right" />
+      <div className=" flex items-center justify-center h-screen bg-blue-50">
+        <Card title="Login" className=" mx-auto max-md: w-1/4">
+          <form onSubmit={handleSubmit} className="p-fluid">
+            <div className="field">
+              <label htmlFor="email">Email</label>
+              <InputText
+                id="email"
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
+              />
+            </div>
+            <br />
+            <div className="field">
+              <label htmlFor="password">Password</label>
+              <Password
+                id="password"
+                name="password"
+                value={formData.password}
+                onChange={handleChange}
+                toggleMask
+                feedback={false}
+              />
+            </div>
+            <br />
+            <Button type="submit" label="Login" className="mt-3" />
+            <br />     
+            <br />       
+            <p className=" text-teal-500">
+              New User?{" "}
+              <span
+                className=" text-red-500 cursor-pointer underline"
+                onClick={() => navigate("/register")}
+              >
+                Register Here
+              </span>{" "}
+            </p>
+          </form>
+        </Card>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
--- Library/src/components/Register.tsx
+++ Library/src/components/Register.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import type { User } from "../interface/User";
 import axios from "axios";
+import { useNavigate  } from "react-router-dom";
 
 // PrimeReact Components
 import { InputText } from "primereact/inputtext";
@@ -10,6 +11,8 @@ import { Message } from "primereact/message";
 import { Card } from "primereact/card";
 
 export const Register = () => {
+  const navigate = useNavigate();
+
   const [formData, setFormData] = useState<User>({
     name: "",
     email: "",
@@ -49,52 +52,54 @@ export const Register = () => {
 
   return (
     <>
-      <Card
-        title="Register"
-        style={{ maxWidth: "400px", margin: "auto", padding: "2rem" }}
-      >
-        <form onSubmit={handleSubmit} className="p-fluid">
-          <div className="field">
-            <label htmlFor="name">Name</label>
-            <InputText
-              id="name"
-              name="name"
-              value={formData.name}
-              onChange={handleChange}
-            />
-          </div>
-
-          <div className="field">
-            <label htmlFor="email">Email</label>
-            <InputText
-              id="email"
-              name="email"
-              value={formData.email}
-              onChange={handleChange}
-            />
-          </div>
-
-          <div className="field">
-            <label htmlFor="password">Password</label>
-            <Password
-              id="password"
-              name="password"
-              value={formData.password}
-              onChange={handleChange}
-              toggleMask
-              feedback={false}
-            />
-          </div>
-
-          <Button type="submit" label="Register" className="mt-3" />
-
-          {message && (
-            <div className="mt-3">
-              <Message severity={message.type} text={message.text} />
+      <div className=" flex items-center justify-center h-screen bg-amber-50">
+        <Card title="Register" className=" mx-auto max-md: w-1/4">
+          <form onSubmit={handleSubmit} className="p-fluid">
+            <div className="field">
+              <label htmlFor="name">Name</label>
+              <InputText
+                id="name"
+                name="name"
+                value={formData.name}
+                onChange={handleChange}
+              />
             </div>
-          )}
-        </form>
-      </Card>
+            <br />
+            <div className="field">
+              <label htmlFor="email">Email</label>
+              <InputText
+                id="email"
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
+              />
+            </div>
+            <br />
+            <div className="field">
+              <label htmlFor="password">Password</label>
+              <Password
+                id="password"
+                name="password"
+                value={formData.password}
+                onChange={handleChange}
+                toggleMask
+                feedback={false}
+              />
+            </div>
+            <br />
+            <Button type="submit" label="Register" className="mt-3" />
+            <br />
+            {message && (
+              <div className="mt-3">
+                <Message severity={message.type} text={message.text} />
+              </div>
+            )}
+            <br />
+            <p className=" text-teal-500">Already a User? <span className=" text-red-500 cursor-pointer underline" onClick={() => navigate("/login")}>Click here to Login</span> </p>
+
+          </form>
+        </Card>
+      </div>
     </>
   );
 };
